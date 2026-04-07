0b3d911188440a3fcb35f33dee5e7c1bcd30f46b
Florian Schieder <florian.schieder@puzzleyou.de>
2026-04-07 14:22:37 +0200

    fix: invalid read could have happened
    valgrind also proved it did. please don't ask why I did not re-run that
    check after supporting more trailing specials (#2). sorry -.-

--- fast_pydb/string/clean_string.c
+++ fast_pydb/string/clean_string.c
@@ -140,7 +140,8 @@ PyObject *processWhitespaces(const char *original, const Py_ssize_t size)
 
     // We must walk a bit back as there still may be trailing specials.
     // First, trim all trailing newlines...
-    while (destination[--loopState.destinationOffset] == '\n') {
+    while (loopState.destinationOffset > 0
+           && destination[--loopState.destinationOffset] == '\n') {
       destination[loopState.destinationOffset] = '\0';
     }
     // ... and only then remove the possible remaining trailing whitespace.
