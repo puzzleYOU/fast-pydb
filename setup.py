8911c09f725de49a6320e6cd9ba8d2ac7e4d25a5
Florian Schieder <florian.schieder@puzzleyou.de>
2026-04-08 06:50:12 +0200

    incremented version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ class ExtraCompileArgsExtension(build_ext):
 
 setup(
     name="fast_pydb",
-    version="1.0.0",
+    version="1.0.1",
     license="MIT",
 
     description="fast implementations of various utility functions",
